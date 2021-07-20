492c0b047070cb41f3e1bd7bd44725e4e43684ad
Braden <kmcbhc@gmail.com>
2021-07-20 11:13:54 +1000

    work

--- index.js
+++ index.js
@@ -1,74 +1,68 @@
-require('dotenv').config();
-
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const Note = require('./models/note');
-
-console.log(Note)
-
-app.use(cors());
-app.use(express.json());
-app.use(express.static('build'));
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const Note = require('./models/note')
 
 
-  app.get('/', (request, response) => {
-      response.send('<h1>Hello There</h1>');
-  });
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
 
-  app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-      response.json(notes)
-    })
-  });
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
-  app.get('/api/notes/:id', (request, response) => {
-      Note.findById(request.params.id).then(note => {
-        response.json(note);
-      });
-  });
+app.use(requestLogger)
 
-  app.delete('/api/notes/:id', (request, response) =>{
-      const id = Number(request.params.id);
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
 
-      notes.filter(note => note.id !== id);
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
-      response.status(204).end();
-  });
-
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
-  
-  app.post('/api/notes', (request, response) => {
-    const body = request.body;
 
-    if (body.content === undefined) {
-      return response.status(404).json({error: 'content missing'});
-    };
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
 
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-    });
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
 
-    note.save().then(savedNote => {
-      response.json(savedNote);
-    });
-  });
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+})
 
-  const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  app.use(unknownEndpoint)
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
 
+  response.status(204).end()
+})
 
-const PORT = process.env.PORT;
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-});
+  console.log(`Server running on port ${PORT}`)
+})
